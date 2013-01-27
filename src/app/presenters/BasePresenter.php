42183a149cb2de6e0c1a4906f7fce49414c0756e
Adam Štipák <adam.stipak@gmail.com>
2013-01-27 12:26:31 +0100

    Adding a Nette Framework sandbox.

---
+++ src/app/presenters/BasePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Nette,
+	Model;
+
+
+/**
+ * Base presenter for all application presenters.
+ */
+abstract class BasePresenter extends Nette\Application\UI\Presenter
+{
+
+}
---
+++ src/app/presenters/HomepagePresenter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Nette,
+	Model;
+
+
+/**
+ * Homepage presenter.
+ */
+class HomepagePresenter extends BasePresenter
+{
+
+	public function renderDefault()
+	{
+		$this->template->anyVariable = 'any value';
+	}
+
+}
